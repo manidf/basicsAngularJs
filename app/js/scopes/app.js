f4740d54424b38f697d6f6d2e67702125ef49569
Mannuel Ferreira <mannuel@wwc.co.za>
2014-06-29 00:37:24 +0200

    refactored chapter names to refelect book

---
+++ app/js/scopes/app.js
@@ -0,0 +1,14 @@
+//angular.module('myApp', [])
+//    .run(function($rootScope){
+//       $rootScope.name = "World";
+//    });
+
+//angular.module("myApp", [])
+//    .controller('MyController', function($scope){
+//        $scope.name = "Manny";
+//    });
+
+angular.module("myApp", [])
+.controller('MyController', function($scope){
+        $scope.name = "Manny";
+    });
