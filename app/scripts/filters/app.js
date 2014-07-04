f7c5a11397aa9678cdb027c0edcb814be5facfb4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-07-04 07:24:23 +0200

    filters updated more examples

--- app/scripts/filters/app.js
+++ app/scripts/filters/app.js
@@ -16,3 +16,31 @@ app.controller('CurrencyDemo', ['$scope', '$filter', function($scope, $filter){
 app.controller('DateDemo', ['$scope', '$filter', function($scope, $filter){
   $scope.today = $filter('date')('[]');
 }]);
+
+angular.module('myApp.filters', [])
+  .filter('capitalize', function(){
+    return function(input) {
+      // input will be the string we pass in
+      if  (input)
+        return input[0].toUpperCase() + input.slice(1);
+    }
+  });
+
+// Custom variables
+angular.module('validationExample', [])
+  .directive('ensureUnique', function($http){
+    require: 'ngModel',
+      link: function(scope, ele, attrs, c) {
+        scope.$watch(attrs.ngModel, function(){
+           $http({
+             method: 'POST',
+             url: '/api/check/' + attrs.ensureUnique,
+             data: {'field': attrs.ensureUnique }
+           }).success(function(data,status,headers,cfg){
+             c.$setValidity('unique', data.isUnique);
+           }).error(function(data,status,headers,cfg){
+             c.$setValidity('unique',false);
+           })
+        });
+    }
+  });
